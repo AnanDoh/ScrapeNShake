013b124827085bbe1218a29a5608bccfeab3380a
Anando <mahbubah.anando.852@my.csun.edu>
2018-03-22 16:19:33 -0700

    added image

--- test.py
+++ test.py
@@ -9,10 +9,13 @@ ol = soup.ol#finds all <ol>
 for things in ol.find_all('a'):
     print(things.get('href')) #gets urls from href="nblah" 
     print(things.text)
+    print('\n')
     anal = things.text
     blob = TextBlob(anal)
     print(blob.tags)#tells you part of speech
+    print('\n')
     print(blob.noun_phrases)#tells you nouns
+    print('\n')
     for sentence in blob.sentences:
         print(sentence.sentiment)#rates how positive or negative a sentence in the title is and if based on personal innfluence
     print('==========================='+'\n')
